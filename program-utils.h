9f830031b3a2404f32d59a1abd78d5db822091f5
Tolga Reis <tolgaareis@gmail.com>
2020-04-19 19:54:26 +0300

    comment was updated

--- program-utils.h
+++ program-utils.h
@@ -2,7 +2,7 @@
 #define PROGRAM_UTILS_H
 
 
-/* calculates the product of two square matrices and returns the result matrix */
+/* calculates the product of two matrices */
 void matrix_mult(const int, const int, const int, const int, const int*, const int*, int*);
 /* gets file descriptors according to given paths */
 void create_fd(int*, int*, const char*, const char*);
